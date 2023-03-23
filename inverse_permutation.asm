21b3b3b0b86a99190212e593a84315deab369ae8
Krzysztof Zyndul <kz438842@students.mimuw.edu.pl>
2023-03-23 19:34:59 +0100

    184

--- inverse_permutation.asm
+++ inverse_permutation.asm
@@ -1,16 +1,13 @@
-;COS equ 0x80000000
-;COS equ 0x7fffffff
 global inverse_permutation
 inverse_permutation:
-        cmp     edi,0                   ; if (n <= 0) return false
+        cmp     edi, 0                  ; if (n <= 0) return false
         jle     bad
 
         mov     r8, rdi
         mov     r9, rsi
         mov     rdi, 0x80000000
         mov     rsi, 0x7fffffff
-
-        cmp     r8,rdi                  ; if (n > INT_MAX) return false
+        cmp     r8, rdi                 ; if (n > INT_MAX) return false
         jg      bad
 
 
@@ -19,28 +16,28 @@ sprawdz_petla:
         movsxd  rax, DWORD [r9+rdx*4]   ; rax = p[i]
         cmp     eax, 0                  ; if (p[i] < 0) return false
         js      bad
-        cmp     rax,r8                  ; if (p[i] > n) return false
+        cmp     rax, r8                 ; if (p[i] > n) return false
         jae     bad
-        add     rdx,0x1                 ; ++i
-        cmp     rdx,r8                  ; while (i < n)
+        add     rdx, 0x1                ; ++i
+        cmp     rdx, r8                 ; while (i < n)
         jne     sprawdz_petla
 
         xor     edx,edx                 ; i = 0
 pierwsza_petla:
         mov     eax, DWORD [r9+rdx*4]   ; eax (dokad) = p[i]
         and     eax, esi                ; eax & 11111111
-        lea     rcx,[r9+rax*4]          ; rcx = p[dokad]
+        lea     rcx, [r9+rax*4]         ; rcx = p[dokad]
         mov     eax, DWORD [rcx]        ; eax = p[dokad]
-        cmp     eax,0                   ; if p[dokad] < 0
+        cmp     eax, 0                  ; if p[dokad] < 0
         js      popraw                  ; petla do poprawy a nie od razu bad
-        or      eax,edi                 ; eax = eax | 1000000000000
-        mov     DWORD [rcx],eax         ; p[dokad] = p[dokad] | 1000000000
-        add     rdx,0x1                 ; ++i;
+        or      eax, edi                ; eax = eax | 1000000000000
+        mov     DWORD [rcx], eax        ; p[dokad] = p[dokad] | 1000000000
+        add     rdx, 0x1                ; ++i;
         cmp     rdx, r8                 ; i < n
         jb      pierwsza_petla
 
 odwroc:
-        xor     edx,edx                 ; i = 0
+        xor     edx, edx                ; i = 0
 odwroc_petla:
         mov     eax, [r9+4*rdx]
         cmp     eax, 0                  ; if (p[i] < 0)
@@ -62,7 +59,7 @@ petla_w_petli:
         mov     edx, ecx                ; i = poprzedni
 
 petla_zakonczeni:
-        add     rdx,0x1                 ; ++i;
+        add     rdx, 0x1                ; ++i;
         cmp     rdx, r8                 ; while (i < n)
         jb      odwroc_petla
 
@@ -71,15 +68,15 @@ end:
         ret
 
 popraw:
-        sub     rdx,0x1
+        sub     rdx, 0x1
         js      bad                     ; imo nie potrzeben
 
 popraw_petla:
-        mov     eax,DWORD  [r9+rdx*4]   ; eax = dokad (p[i])
-        sub     rdx,0x1                 ; --i
-        and     eax,esi                 ; dokad = dokad & 1111111
-        and     DWORD  [r9+rax*4],esi   ; p[dokad] = p[dokad] & 111111
-        test    edx,edx                 ; if (i >= 0)
+        mov     eax, DWORD  [r9+rdx*4]  ; eax = dokad (p[i])
+        sub     rdx, 0x1                ; --i
+        and     eax, esi                ; dokad = dokad & 1111111
+        and     DWORD  [r9+rax*4], esi  ; p[dokad] = p[dokad] & 111111
+        test    edx, edx                ; if (i >= 0)
         jns     popraw_petla
 
 bad:
