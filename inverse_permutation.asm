76871bbe042fe56942d631a02c40db659307a62c
Krzysztof Zyndul <kz438842@students.mimuw.edu.pl>
2023-03-23 18:23:33 +0100

    Sformatowane

--- inverse_permutation.asm
+++ inverse_permutation.asm
@@ -26,55 +26,53 @@ sprawdz_petla:
         xor     edx,edx                 ; i = 0
 pierwsza_petla:
         mov     eax, DWORD [r9+rdx*4]   ; eax (dokad) = p[i]
-        and     eax, esi                 ; eax & 11111111
-        lea     rcx,[r9+rax*4]         ; rcx = p[dokad]
-        mov     eax, DWORD [rcx]         ; eax = p[dokad]
-        cmp     eax,0                     ; if p[dokad] < 0
-        js      popraw                       ; petla do poprawy a nie od razu bad
-        or      eax,edi              ; eax = eax | 1000000000000
-        mov     DWORD [rcx],eax              ; p[dokad] = p[dokad] | 1000000000
-        add     rdx,0x1                      ; ++i;
-        cmp     rdx, r8                    ;  i < n
+        and     eax, esi                ; eax & 11111111
+        lea     rcx,[r9+rax*4]          ; rcx = p[dokad]
+        mov     eax, DWORD [rcx]        ; eax = p[dokad]
+        cmp     eax,0                   ; if p[dokad] < 0
+        js      popraw                  ; petla do poprawy a nie od razu bad
+        or      eax,edi                 ; eax = eax | 1000000000000
+        mov     DWORD [rcx],eax         ; p[dokad] = p[dokad] | 1000000000
+        add     rdx,0x1                 ; ++i;
+        cmp     rdx, r8                 ; i < n
         jb      pierwsza_petla
         jmp     odwroc
 popraw:
         sub     rdx,0x1
-        js      bad      ; imo nie potrzeben
+        js      bad                     ; imo nie potrzeben
 popraw_petla:
-        mov     eax,DWORD  [r9+rdx*4] ; eax = dokad (p[i])
-        sub     rdx,0x1                ; --i
-        and     eax,esi         ; dokad = dokad & 1111111
-        and     DWORD  [r9+rax*4],esi ; p[dokad] = p[dokad] & 111111
-        test    edx,edx                      ; if (i >= 0)
+        mov     eax,DWORD  [r9+rdx*4]   ; eax = dokad (p[i])
+        sub     rdx,0x1                 ; --i
+        and     eax,esi                 ; dokad = dokad & 1111111
+        and     DWORD  [r9+rax*4],esi   ; p[dokad] = p[dokad] & 111111
+        test    edx,edx                 ; if (i >= 0)
         jns     popraw_petla
         jmp     bad
 
 odwroc:
-        xor     edx,edx  ; i = 0
+        xor     edx,edx                 ; i = 0
 odwroc_petla:
-                ; if (p[i] < 0)
         mov     eax, [r9+4*rdx]
-        cmp     eax, 0
+        cmp     eax, 0                  ; if (p[i] < 0)
         jge     petla_zakonczeni
-        mov     rcx, rdx    ; poprzedni = i
-        mov     eax, DWORD [r9+4*rdx] ; eax = p[i]
-        and     eax, esi ;eax = eax & 1111
-        mov     edx, eax ; i = p[i]
+        mov     rcx, rdx                ; poprzedni = i
+        mov     eax, DWORD [r9+4*rdx]   ; eax = p[i]
+        and     eax, esi                ; eax = eax & 1111
+        mov     edx, eax                ; i = p[i]
 
 petla_w_petli:
-        mov     edi, [r9+4*rdx] ; temp = p[i]
-        and     edi, esi ; temp = temp & 111111
-        mov     DWORD [r9+4*rdx], ecx ; p[i] = poprzedni
-        mov     ecx, edx ; poprzedni = i
-        mov     edx, edi ; i  = temp
-        mov     eax, [r9+4*rdx] ; eax = p[i]
-        cmp     eax, 0 ; while (p[i] < 0)
+        mov     edi, [r9+4*rdx]         ; temp = p[i]
+        and     edi, esi                ; temp = temp & 111111
+        mov     DWORD [r9+4*rdx], ecx   ; p[i] = poprzedni
+        mov     ecx, edx                ; poprzedni = i
+        mov     edx, edi                ; i  = temp
+        mov     eax, [r9+4*rdx]         ; eax = p[i]
+        cmp     eax, 0                  ; while (p[i] < 0)
         jl      petla_w_petli
-        mov     edx, ecx ; i = poprzedni
+        mov     edx, ecx                ; i = poprzedni
 
 petla_zakonczeni:
-        add     rdx,0x1                      ; ++i;
-
+        add     rdx,0x1                 ; ++i;
         cmp     rdx, r8                 ; while (i < n)
         jb      odwroc_petla
 
