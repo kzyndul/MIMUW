92c39597bfd6a5188b37b98ad1dee0e9abb15707
Krzysztof Zyndul <kz438842@students.mimuw.edu.pl>
2023-03-23 21:22:21 +0100

    87

--- inverse_permutation.asm
+++ inverse_permutation.asm
@@ -1,65 +1,73 @@
 global inverse_permutation
 inverse_permutation:
-        test    edi, edi                ; if (n <= 0) return false
+        test    edi, edi                ; sprawdzam czy n > 0
         js      bad
 
-        mov     r8, rdi
-        mov     r9, rsi
-        mov     rdi, 0x80000000
-        mov     rsi, 0x7fffffff
-        cmp     r8, rdi                 ; if (n > INT_MAX) return false
-        jg      bad
-
+        mov     r8, 0x80000000          ; przenosze dwie stale do rejestrow
+        mov     r9, 0x7fffffff
+        cmp     rdi, r8                 ; sprawdzam czy n < INT_MAX + 2 dla n
+        jg      bad                     ; wiekszych funkcjia zawsze powinna
+                                        ; zwrocic false
         xor     rdx, rdx                ; i = 0
 sprawdz_petla:
-        mov     eax, DWORD [r9+rdx*4]   ; rax = p[i]
-        test    eax, eax                ; if (p[i] < 0) return false
+        movsxd  rax, DWORD [rsi+rdx*4]  ; rax = p[i]
+        test    eax, eax                ; sprawdzam czy p[i] >= 0
         js      bad
-        cmp     eax, r8d                ; if (p[i] > n) return false
+
+        cmp     eax, edi                ; sprawdzam czy p[i] < n
         jae     bad
+
         inc     edx                     ; ++i
-        cmp     edx, r8d                ; while (i < n)
+        cmp     edx, edi                ; while (i < n)
         jne     sprawdz_petla
 
         xor     edx,edx                 ; i = 0
+
+
+; sprawdzam czy tablica p jest poprawna permutacja. Przechodze przez cala
+; tablice gdy jestem na pozycji i to zapalam ostatni znak liczbie na pozycji
+; p[p[i]] co oznacza ze juz ja spotkalem w permutacji.
 pierwsza_petla:
-        mov     eax, DWORD [r9+rdx*4]   ; eax (dokad) = p[i]
-        and     eax, esi                ; eax & 11111111
-        lea     rcx, [r9+rax*4]         ; rcx = p[dokad]
-        mov     eax, DWORD [rcx]        ; eax = p[dokad]
-        test    eax, eax                ; if p[dokad] < 0
-        js      popraw                  ; petla do poprawy a nie od razu bad
-        or      eax, edi                ; eax = eax | 1000000000000
-        mov     DWORD [rcx], eax        ; p[dokad] = p[dokad] | 1000000000
-        inc     edx                     ; ++i;
-        cmp     edx, r8d                ; i < n
+        mov     eax, DWORD [rsi+rdx*4]  ; eax = p[i]
+        and     eax, r9d                ; pozbywam sie oznaczenia
+        lea     rcx, [rsi+rax*4]        ; rcx = &p[p[i]]
+        mov     eax, DWORD [rcx]        ; eax = p[]
+        test    eax, eax                ; sprawdzam czy p[i] bylo juz
+                                        ; odwiedzone
+        js      popraw
+        or      eax, r8d                ; zaznaczam jako odwiedzone
+        mov     DWORD [rcx], eax        ; zapisuje do tablicy
+        inc     edx                     ; ++i
+        cmp     edx, edi                ; while (i < n)
         jb      pierwsza_petla
 
 odwroc:
         xor     edx, edx                ; i = 0
+; petla odwracajaca permutacje. Jezeli jakas liczba jest ujemna to oznacza,
+; ze jeszcze nie zostala odwrucona.
 odwroc_petla:
-        mov     eax, [r9+4*rdx]
-        cmp     eax, 0                  ; if (p[i] < 0)
-        jge     petla_zakonczeni
-        mov     rcx, rdx                ; poprzedni = i
-        mov     eax, DWORD [r9+4*rdx]   ; eax = p[i]
-        and     eax, esi                ; eax = eax & 1111
+        mov     eax, [rsi+4*rdx]        ; eax = p[i]
+        test     eax, eax                  ; if (p[i] < 0) nie odwrocony element
+        jns     petla_zakonczeni
+        mov     ecx, edx                ; poprzedni = i
+        mov     eax, DWORD [rsi+4*rdx]  ; eax = p[i]
+        and     eax, r9d                ; eax = eax & 1111
         mov     edx, eax                ; i = p[i]
 
 petla_w_petli:
-        mov     edi, [r9+4*rdx]         ; temp = p[i]
-        and     edi, esi                ; temp = temp & 111111
-        mov     DWORD [r9+4*rdx], ecx   ; p[i] = poprzedni
+        mov     r8d, [rsi+4*rdx]        ; temp = p[i]
+        and     r8d, r9d                ; temp = temp & 111111
+        mov     DWORD [rsi+4*rdx], ecx  ; p[i] = poprzedni
         mov     ecx, edx                ; poprzedni = i
-        mov     edx, edi                ; i  = temp
-        mov     eax, [r9+4*rdx]         ; eax = p[i]
-        cmp     eax, 0                  ; while (p[i] < 0)
-        jl      petla_w_petli
+        mov     edx, r8d                ; i  = temp
+        mov     eax, [rsi+4*rdx]        ; eax = p[i]
+        test    eax, eax                ; while (p[i] < 0)
+        js      petla_w_petli
         mov     edx, ecx                ; i = poprzedni
 
 petla_zakonczeni:
         inc     edx                     ; ++i;
-        cmp     edx, r8d                ; while (i < n)
+        cmp     edx, edi                ; while (i < n)
         jb      odwroc_petla
 
 end:
@@ -70,9 +78,9 @@ popraw:
         dec     edx
 
 popraw_petla:
-        mov     eax, DWORD  [r9+rdx*4]  ; eax = dokad (p[i])
-        and     eax, esi                ; dokad = dokad & 1111111
-        and     DWORD  [r9+rax*4], esi  ; p[dokad] = p[dokad] & 111111
+        mov     eax, DWORD  [rsi+rdx*4] ; eax = dokad (p[i])
+        and     eax, r9d                ; dokad = dokad & 1111111
+        and     DWORD  [rsi+rax*4], r9d ; p[dokad] = p[dokad] & 111111
         dec     edx                     ; --i
         test    edx, edx                ; if (i >= 0)
         jns     popraw_petla
